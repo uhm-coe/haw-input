145548514078bebd0bab6c004bd1b6121aafb752
Patrick Karjala <pkarjala@gmail.com>
2025-09-09 14:59:12 -1000

    Remove testing code.  Add function descriptions.  Rename hawCharacterReplace to hawiCharacterReplace.

--- haw-input.js
+++ haw-input.js
@@ -1,47 +1,37 @@
 /**
  * hawInput
- * @param {*} element_id 
- * @returns 
+ * 
+ * Binds a listener to an input element specified by the ID of that element.
+ * When the chord key ` is typed, allows output of an ʻokina or kahakō.
+ * 
+ * @param {String} element_id The ID of the input or textarea to bind to.
+ * @returns {boolean} True if the binding succeeds (currently it always should)
  */
 function hawInput(element_id) {
-  // Set values for input.
+  // Set hash of values for input replacement.
   const HAWI_BACKTICK_KEYS = {"'":"ʻ", "a":"ā", "e":"ē", "i":"ī", "o":"ō", "u":"ū","A":"Ā", "E":"Ē", "I":"Ī", "O":"Ō", "U":"Ū"}
   // Set input element; maybe change this to work with classes?
   const text_input = document.getElementById(element_id);
+  // Flag for whether the backtick key has been pressed.
   let backtick_flag = false;
-  // Listen for keypresses to 
+  // Listen for keypresses within the provided element.
   text_input.addEventListener('keypress', (event) => {
     const key = event.key;
-    // console.log(key);
     // Check and verify one of the keys is a chordkey if one is not set.
-    // if( !backtick_flag && !backslash_flag ) {
     if( !backtick_flag ) {
       if(key === '`') {
         // Backtick chord key matched, do not type.
-        // console.log("Match to chord key `!");
         event.preventDefault();
         backtick_flag = true;
         return false;
       } 
-      // else if( key === '\\') {
-      //   // Backslash chord key matched, do not type.
-      //   // console.log("Match to chord key \\!");
-      //   event.preventDefault();
-      //   backslash_flag = true;
-      //   return false;
-      // }
     } else {
       // A chord key is set, so process the next key input.
       let replacement_character = '';
       if(backtick_flag) {
-        replacement_character = haiCharacterReplace(key, HAWI_BACKTICK_KEYS);
+        replacement_character = hawiCharacterReplace(key, HAWI_BACKTICK_KEYS);
       } 
-      // else if (backslash_flag) {
-      //   replacement_character = haiCharacterReplace(key, HAI_BACKSLASH_KEYS);
-      // }
-      // console.log(replacement_character);
-      // If either flag is set, and if the character was replaced, use it in the text.
-      // if( (backtick_flag || backslash_flag) && (replacement_character != key) ) {
+      // If the flag is set, and if the character was replaced, use it in the text.
       if( (backtick_flag) && (replacement_character != key) ) {
         // Prevent the default character from being typed.
         event.preventDefault();
@@ -51,21 +41,27 @@ function hawInput(element_id) {
         let val = text_input.value;
         text_input.value = val.slice(0, start) + replacement_character + val.slice(end);
         text_input.selectionStart = text_input.selectionEnd = start + 1;
-        // Reset the flags
-        // backtick_flag = backslash_flag = false;
+        // Reset the flag
         backtick_flag = false;
         return false;
       } else {
-        // Reset the flags
-        // backtick_flag = backslash_flag = false;
+        // Reset the flag and allow normal character typing.
         backtick_flag = false;
       }
     }
-    // console.log(backtick_flag);
-    // console.log(backslash_flag);
   });
 
-  function haiCharacterReplace(input_character, replacement_object) {
+  /**
+   * hawiCharacterReplace
+   * 
+   * Checks an input character against a hash.  If there is a match, returns the replacement character.
+   * If not, returns the original character.
+   * 
+   * @param {String} input_character A single character to be checked if it should be replaced
+   * @param {Object} replacement_object An object of the key:replacement pairs to be checked.
+   * @returns {String} Either the original single character or the replacement if a match was found.
+   */
+  function hawiCharacterReplace(input_character, replacement_object) {
    return replacement_object[input_character] ? replacement_object[input_character] : input_character;
   }
 
